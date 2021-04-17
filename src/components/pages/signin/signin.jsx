3e0e5f86b2993aa158a284474b82abceef1e42a2
emmanz95 <emmanza2@gmail.com>
2021-04-17 18:51:08 +0100

    removed an unwanted import from signin.jsx

--- src/components/pages/signin/signin.jsx
+++ src/components/pages/signin/signin.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/Styles';
 import { StyledButton, StyledContainer } from './signinStyles';
